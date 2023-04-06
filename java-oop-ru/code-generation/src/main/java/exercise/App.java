bffa9c2c816344cf8bb89428667fc80e1b69d188
@hexlet/cli <support@hexlet.io>
2023-04-06 12:50:19 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+// BEGIN
+public class App {
+    public static void save(Path path, Car car) {
+        try {
+            Files.writeString(path, car.serialize());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Car extract(Path path) {
+        try {
+            var content = Files.readString(path);
+            return Car.deserialize(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+// END
