cbcd78ae0e9fc7c1d1396666fdff1d98b8eb19ad
@hexlet/cli <support@hexlet.io>
2023-05-22 13:00:47 +0300

    submit java-web-ru/rest-api

---
+++ java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
@@ -0,0 +1,60 @@
+package exercise.controllers;
+
+import io.javalin.http.Context;
+import io.javalin.apibuilder.CrudHandler;
+import io.ebean.DB;
+import java.util.List;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.lang3.StringUtils;
+
+public class UserController implements CrudHandler {
+
+    public void getAll(Context ctx) {
+        // BEGIN
+        List<User> users = new QUser()
+                .orderBy()
+                    .id.asc()
+                .findList();
+        ctx.json(DB.json().toJson(users));
+        // END
+    };
+
+    public void getOne(Context ctx, String id) {
+
+        // BEGIN
+        User user = new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .findOne();
+        ctx.json(DB.json().toJson(user));
+        // END
+    };
+
+    public void create(Context ctx) {
+
+        // BEGIN
+        User user = DB.json().toBean(User.class, ctx.body());
+        user.save();
+        // END
+    };
+
+    public void update(Context ctx, String id) {
+        // BEGIN
+        User user = DB.json().toBean(User.class, ctx.body());
+        user.setId(id);
+        user.update();
+        // END
+    };
+
+    public void delete(Context ctx, String id) {
+        // BEGIN
+
+        new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .delete();
+        // END
+    };
+}
