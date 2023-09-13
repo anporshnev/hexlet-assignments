f25b86a2f4e473935c3d3fc76fd59dc0041fb958
@hexlet/cli <support@hexlet.io>
2023-09-13 18:03:33 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,58 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Collections;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+
+        // BEGIN
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+
+        app.post("/users", ctx -> {
+           var firstName = ctx.formParam("firstName").trim();
+           var lastName = ctx.formParam("lastName").trim();
+           var email = ctx.formParam("email").trim().toLowerCase();
+           var password = ctx.formParam("password");
+
+           String capitalizedFirstName = StringUtils.capitalize(firstName);
+           String capitalizedLastName = StringUtils.capitalize(lastName);
+           String encryptedPassword = Security.encrypt(password);
+
+           User user = new User(capitalizedFirstName, capitalizedLastName, email, encryptedPassword);
+           UserRepository.save(user);
+           ctx.redirect("/users");
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
